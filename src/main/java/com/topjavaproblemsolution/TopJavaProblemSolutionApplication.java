1ae787c4c69aa5ffde57d859aa80677a292ff79f
manish-7span <manish@7span.com>
2024-07-13 17:33:12 +0530

    Start Project : Initial Commit

---
+++ src/main/java/com/topjavaproblemsolution/TopJavaProblemSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.topjavaproblemsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopJavaProblemSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopJavaProblemSolutionApplication.class, args);
+    }
+
+}
