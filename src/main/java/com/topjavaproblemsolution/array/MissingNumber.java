52e68116df1fcdc82a5b41e16bbbcf46deeb26c7
manish-7span <manish@7span.com>
2024-07-15 10:57:17 +0530

    Array : Update code that even more than one number is Missing still give a All the missing numbers with both Boolean and hasSet.

--- src/main/java/com/topjavaproblemsolution/array/MissingNumber.java
+++ src/main/java/com/topjavaproblemsolution/array/MissingNumber.java
@@ -1,12 +1,18 @@
 package com.topjavaproblemsolution.array;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /*
 *  Find missing number in the array.
 */
 public class MissingNumber {
     public static void main(String[] args) {
+        // If Only One number is Missing.
+       /*
         int arr2[] = new int []{1, 2, 3, 4, 6};
-
 
         int n = arr2.length+1;
         int totalSum = n * (n+1)/2;
@@ -18,5 +24,55 @@ public class MissingNumber {
 
         int missingNumber = totalSum - arraySum;
         System.out.println("Missing Number is => "+missingNumber);
+
+
+        */
+
+
+//      If more than One number is Missing.
+
+        int arr2[] = new int[] {1,2,5,8,10};
+        List<Integer> missingNumber = new ArrayList<>(); // "<>" declare that type of list declare on LHS.
+
+        int maxValue = arr2[0];
+        for(int num:arr2){
+            if(num > maxValue){
+                maxValue = num;
+            }
+        }
+
+        boolean[] present = new boolean[maxValue+1];
+
+        for(int num : arr2){
+                present[num]=true;
+        }
+
+        for(int i=1;i<=maxValue;i++){
+            if(!present[i]){
+                missingNumber.add(i);
+            }
+        }
+
+        /*
+        * Using a HashSet to track elements present in the array.
+        * This allows efficient checking and storage of unique elements,
+        * ensuring only missing numbers are added to the `missingNumber` list.
+
+        Set<Integer> present = new HashSet<>();
+
+        for (int num : arr2) {
+            present.add(num);
+        }
+
+        for (int i = 1; i <= maxValue; i++) {
+            if (!present.contains(i)) {
+                missingNumber.add(i);
+            }
+        }
+         */
+
+        for(int num:missingNumber){
+            System.out.println(num);
+        }
     }
 }
